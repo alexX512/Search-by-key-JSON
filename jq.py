b49df7aebc1b1826222172caf4b30dadbfe1f509
alexX512 <61140353+alexX512@users.noreply.github.com>
2020-11-16 00:12:11 +0300

    Decompose program into functions

--- jq.py
+++ jq.py
@@ -1,11 +1,12 @@
-import sys
 import os
+import sys
 import json
 import logging
+import argparse
 
 
 # Log levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
-LOG_LEVEL = os.environ['LOG_LEVEL'] if 'LOG_LEVEL' in os.environ else "INFO"
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
 def to_real_key(key):
@@ -14,34 +15,54 @@ def to_real_key(key):
     return key
 
 
-def get_logger(name):
-    logger = logging.getLogger(name)
+def get_args():
+    parser = argparse.ArgumentParser(description='Ping script')
+    parser.add_argument('json_file', action="store")
+    parser.add_argument('composite_key', action="store")
+    return parser.parse_args()
+
+
+def get_logger():
+    logger_name = os.path.splitext(os.path.basename(__file__))[0]
+    logger = logging.getLogger(logger_name)
     logger.setLevel(logging.getLevelName(LOG_LEVEL))
-    fh = logging.FileHandler(name + ".log")
+
+    fh = logging.FileHandler(logger_name + ".log")
     fh.setFormatter(logging.Formatter(
         '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s'))
+
     logger.addHandler(fh)
     return logger
 
 
-logger_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
-logger = get_logger(logger_name)
-
-json_file = sys.argv[1]
-composite_key = sys.argv[2]
-logger.info("Find key: " + composite_key)
-
-with open(json_file, "r") as f:
+def get_json_value(json_file, composite_key, logger):
     logger.info("Start")
-    json_format = json.load(f)
+
+    json_format = json.load(json_file)
     json_dict = json_format
+
     single_keys = filter(lambda x: x != '', composite_key.split('.'))
     for i in single_keys:
         try:
             json_dict = json_dict[to_real_key(i)]
         except Exception as e:
-            logger.debug("There is no key '" + i + "'")
+            logger.debug(f"There is no key '{i}'")
             logger.exception("%s", e)
             sys.exit(1)
-    print(json.dumps(json_dict, indent=4))
+
     logger.info("Successful finish!")
+    return json.dumps(json_dict, indent=4)
+
+
+def print_json_value():
+    args = get_args()
+    logger = get_logger()
+
+    logger.info("Find key: " + args.composite_key)
+
+    with open(args.json_file, "r") as f:
+        print(get_json_value(f, args.composite_key, logger))
+
+
+if __name__ == '__main__':
+    print_json_value()
